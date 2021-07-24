9c86e5e5531bf925b1d6225428041fcaa02ad5f4
eric-freitas <eric.freitas.matos@gmail.com>
2021-07-24 15:36:18 -0300

    novo elemento visual para expandir e comprimir os detalhes das experiencias

--- web/src/components/ExperienceItem/index.tsx
+++ web/src/components/ExperienceItem/index.tsx
@@ -4,23 +4,33 @@ import ReactMarkdown from 'react-markdown'
 import './index.scss';
 import ExperienceAttribution from '../ExperienceAttribution';
 import { ExperienceData } from '../../models/Experience';
+import { useState } from 'react';
 
 
 const ExperienceItem:React.FC<ExperienceData> = (props) => {
     const { company, conclusion, start, position, detail, attribution } = props;
 
-    const mdDetail = (detail && <ReactMarkdown className="experience-item__detail">{detail}</ReactMarkdown>) || null
+    const [ expanded, setExpanded ] = useState<boolean>(true);
+
+    const mdDetail = (detail && expanded && <ReactMarkdown className="experience-item__detail">{detail}</ReactMarkdown>) || null
     const attributionList = 
         (   attribution && 
+            expanded &&
             attribution.length && 
             (<ul className="experience-item__attribution">{attribution.map((item, index) => <ExperienceAttribution key={index} {...item} />)}</ul>)
         ) || null;
 
+    const className = `experience-item ${expanded ? "--expanded" : ""}`;
+
     return (
-        <div className="experience-item">
+        <div className={className}>
             <div className="experience-item__duration">
                 <h4><ReactMarkdown className="exp-item__title">{company}</ReactMarkdown></h4>
                 <span>{start}-{conclusion}</span>
+            </div>
+            <div className="experience-item__splitter">
+                <div className="experience-item__bar"></div>
+                <div className="experience-item__dot" onClick={()=>setExpanded(!expanded)}></div>
             </div>
             <div className="experience-item__content">
                 <span className="experience-item__position">{position}</span>
